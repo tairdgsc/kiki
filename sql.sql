6a6d829ed3781b38dbf7d115f914f34bc678bed8
Taila Rodrigues <taiciric@gmail.com>
2022-11-20 16:28:12 -0300

    Finalizei a AC2

---
+++ sql.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE  IF NOT EXISTS `banco_kiki`;
+USE `banco_kiki`;
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `idusuario` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `email` VARCHAR(255) NULL,
+  `senha` VARCHAR(255) NULL,
+  PRIMARY KEY (`idusuario`))
+ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `cadastro_prestadores`;
+CREATE TABLE `cadastro_prestadores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `cpf` varchar(45) NOT NULL,
+  `sexo` varchar(45) NOT NULL,
+  `endereco` varchar(100) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `estado` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
---
+++ src/model/Prestador.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+
+/**
+ *
+ * @author Taila Rodrigues
+ */
+public class Prestador {
+    
+    //atributos
+    private String nome = "";
+    private String cpf = "";
+    private String sexo = "";
+    private String endereco = "";
+    private String cidade = "";
+    private String estado = "";
+    
+    //GET e SET
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    public void limpaCliente(){
+       nome = "";
+       cpf = "";
+       sexo = "";
+       endereco = "";
+       cidade = "";
+       estado = "";
+    }   
+    
+}
